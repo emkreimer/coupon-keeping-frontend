5d27b19c3c09b78818200fc0b080e969636e6394
Eduarda Kreimer <eduardakreimer@gmail.com>
2023-07-31 16:34:49 -0300

    Returning data from API

---
+++ coupon-client/src/components/Products.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container mx-auto">
+    <container fluid>
+      <b-row dense>
+        <b-col
+          v-for="item in products"
+          :key="item.id"
+          cols="12"
+          xs="6"
+          sm="4"
+          md="3"
+          lg="2"
+        ></b-col>
+      </b-row>
+    </container>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Products',
+  props: ['products']
+}
+</script>

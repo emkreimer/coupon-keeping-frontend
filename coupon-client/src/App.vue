8a7845e71bb56c5f1dca10461588f4f1c16c5b9e
Eduarda Kreimer <eduardakreimer@gmail.com>
2023-08-03 11:18:01 -0300

    Adding sass

--- coupon-client/src/App.vue
+++ coupon-client/src/App.vue
@@ -2,6 +2,12 @@
 import { onMounted, ref } from 'vue'
 import axios from 'axios'
 
+const fields = ref([
+  { key: 'label', label: 'Nome' },
+  { key: 'price', label: 'Preço' },
+  { key: 'supply', label: 'Estoque' }
+])
+
 const page = ref({
   nPage: 1,
   size: 10,
@@ -11,14 +17,13 @@ const page = ref({
 
 onMounted(() => {
   axios.get('http://localhost:8080/coupons/product/list').then((res) => {
-    console.log(res.data)
     page.value.products = res.data
   })
 })
 </script>
 
 <template>
-  <router-view :products="page.products"></router-view>
+  <router-view :products="page.products" :fields="fields"></router-view>
 </template>
 
 <script>
@@ -26,3 +31,10 @@ export default {
   name: 'App'
 }
 </script>
+<style lang="scss">
+#app {
+  max-width: 1280px;
+  display: flex;
+  flex-direction: column;
+}
+</style>
--- coupon-client/src/components/Products.vue
+++ coupon-client/src/components/Products.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="container mx-auto">
-    <div class="row">
-      <div class="col">heyy</div>
-      <div class="col" v-for="product in products" :key="product.id">teste</div>
-      <div class="col">heyy</div>
-    </div>
+  <div>
+    <table class="table table-striped">
+      <thead>
+        <th v-for="field in fields" :key="field">{{ field.label }}</th>
+      </thead>
+      <tbody>
+        <tr v-for="product in products" :key="product.id">
+          <td v-for="field in fields" :key="field.key">{{ product[field.key] }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 <script>
 export default {
   name: 'Products',
-  props: ['products']
+  props: ['products', 'fields']
 }
 </script>
